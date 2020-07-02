6bd4acc31d35c503a625c97a5f6a0b0d368da14d
hmdraza9 <hmdraza9@gmail.com>
2020-07-03 01:36:11 +0530

    first commit for this folder

---
+++ src/main/java/com/test/selenium/maven/regressionTesting/App.java
@@ -0,0 +1,13 @@
+package com.test.selenium.maven.regressionTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
